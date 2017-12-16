a06d00adbfb2e07f4e24161bedf6e847146c3f10
Eugene Chen <eugene0520@users.noreply.github.com>
2017-12-16 12:18:51 +0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -72,7 +72,7 @@ function Snake() {
   }
 
   this.show = function () {
-    fill(255);
+    fill(random(255),random(255),random(255));
     for (var i = 0; i < this.tail.length; i++) {
       rect(this.tail[i].x, this.tail[i].y, scl, scl);
     }
